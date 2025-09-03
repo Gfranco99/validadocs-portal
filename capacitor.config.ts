0c1560336bdd98379c2d064e5d8a904e031cd5b7
Guilherme Franco <gfranco@integral.com.br>
2025-09-03 13:49:34 -0300

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'validadocs-portal',
+  webDir: 'www'
+};
+
+export default config;
