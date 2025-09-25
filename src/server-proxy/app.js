e91e39a9791d07d9b37d3725a148b3ebe9677de0
mnunes <mnunes@albacore.com.br>
2025-09-25 17:34:11 -0300

    Implementação do log e registro das validações por usuario

--- src/server-proxy/app.js
+++ src/server-proxy/app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const auth = require("./controller/auth.controller");
+const { logEvent } = require("./infrastructure/log/log.service");
 
 const app = express();
 app.use(express.json());
@@ -17,6 +18,8 @@ const upload = multer({ dest: 'uploads/' });
 
 app.post('/verify', upload.single('file'), async (req, res) => {
   try {
+    
+    const userId = req.body.userid;
     const filePath = req.file.path;
     const fileStream = fs.createReadStream(filePath);
 
@@ -46,6 +49,9 @@ app.post('/verify', upload.single('file'), async (req, res) => {
 
     // Limpa arquivo temporário
     fs.unlinkSync(filePath);
+
+    // Registra log do evento
+    await logEvent(userId, "VERIFY_DOCUMENT", { document: response.data });
 
     res.json(response.data);
 
