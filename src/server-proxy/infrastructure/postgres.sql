90c562b892a2dd4b57074d8526b592a79ef95303
mnunes <mnunes@albacore.com.br>
2025-09-25 15:43:22 -0300

    Ajuste de script

--- src/server-proxy/infrastructure/postgres.sql
+++ src/server-proxy/infrastructure/postgres.sql
@@ -1,6 +1,7 @@
-CREATE TABLE credentials (
+CREATE TABLE validadocscredentials (
     id SERIAL PRIMARY KEY,
     user_id UUID NOT NULL DEFAULT gen_random_uuid(),
+    cliente VARCHAR(255) NOT NULL,
     token VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     expires_at TIMESTAMPTZ NULL,
